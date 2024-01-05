a829b09237e9297689fe7d27d2e85d4e34fe5fad
ryo2141 <ryo.miyauchi.2703.19@gmail.com>
2024-01-06 01:37:50 +0900

    dockerコンテナ上で起動

--- go-app/main.go
+++ go-app/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Printf("Hello World\n")
+	http.HandleFunc("/", echoHello)
+	http.ListenAndServe(":8000", nil)
+}
+
+func echoHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello World</h1>")
 }
