b25852f668fee4595b547f43eac652ccccef8085
ryo2141 <ryo.miyauchi.2703.19@gmail.com>
2024-01-06 11:07:10 +0900

    docker起動OK

--- react-ts-app/.eslintrc.js
+++ react-ts-app/.eslintrc.js
@@ -5,29 +5,19 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "react-app",
-    "react-app/jest",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "prettier",
+    'react-app',
+    'react-app/jest',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
   ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
-    },
-  ],
-  parser: "@typescript-eslint/parser",
+  overrides: [],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint", "react"],
+  plugins: ['@typescript-eslint', 'react'],
   rules: {},
-};
+}
